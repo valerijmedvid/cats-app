ed081a59b17531cce950017ba4d3327eb22a658e
Valerij Medviď <vmedvid@gmail.com>
2022-03-17 14:28:36 +0100

    Vue3 clean install

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue"
+
+export default {
+  components: { TheWelcome },
+}
+</script>
+
+<style scoped></style>
