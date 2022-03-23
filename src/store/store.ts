a0e701735add24a1cfb97e6097ae41909470bd9d
Valerij Medviď <vmedvid@gmail.com>
2022-03-23 07:37:36 +0100

    types into store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,15 +2,20 @@ import { createStore } from "vuex"
 
 const user = JSON.parse(localStorage.getItem("cats_user") || "{}")
 
+interface Auth {
+  token: string
+  expiresIn: number | null
+}
+
 export const store = createStore({
-  state() {
+  state(): Auth {
     return {
       token: user.token,
       expiresIn: user.expiresIn,
     }
   },
   mutations: {
-    login(state, user_token) {
+    login(state: Auth, user_token) {
       const now = new Date()
       const expire = now.setSeconds(user_token.expires_in)
 
@@ -22,17 +27,17 @@ export const store = createStore({
         JSON.stringify({ token: `Bearer ${user_token.access_token}`, expiresIn: expire })
       )
     },
-    logout(state) {
+    logout(state: Auth) {
       state.token = ""
-      state.expiresIn = ""
+      state.expiresIn = null
 
       localStorage.removeItem("cats_user")
     },
   },
   getters: {
-    isLogged(state) {
-      const now = new Date()
-      return state.expiresIn > now
+    isLogged(state: Auth) {
+      const now: number = +new Date()
+      return state.expiresIn ? state.expiresIn > now : false
     },
   },
 })
